694b3e1e299fb16fa04b5b2d72e3286f455e9af8
Can Aknesil <canaknesil@Cans-MacBook-Air-5.local>
2019-10-20 14:35:17 +0200

    Conversion of list of numbers to and from genetic string.

--- main.py
+++ main.py
@@ -20,3 +20,13 @@ gen_str_list = [bytearray(3), bytearray(3), bytearray(3)]
 gen_str_list = gen.next_n_generation(gen_str_list, 4, 4, 3, 50,
                                      lambda gs: gs[0]+gs[1]+gs[2])
 print(gen_str_list)
+
+list_with_size = [[3.3 , 4], [4, 8]]
+print(list_with_size)
+gs = ver.list_with_size_to_gs(list_with_size)
+print(gs)
+received_lws = ver.gs_to_list_with_size(gs, list_with_size)
+print(received_lws)
+
+
+
--- verification.py
+++ verification.py
@@ -1,6 +1,8 @@
 # Verification code
 
 import genetic as gen
+import struct
+import copy
 
 #
 # UTILITY
@@ -11,6 +13,7 @@ def print_bytearray(arr):
         print(hex(b), ' ', end='')
     print('')
 
+# TODO make grey work with bytes
 def encode_grey(n):
     return n ^ (n >> 1)
 
@@ -20,6 +23,55 @@ def decode_grey(n):
         n = n ^ mask
         mask = mask >> 1
     return n
+
+# Float only bocomes 4 bytes.
+def get_grey_bytes(item, n):
+    data = 0;
+    if isinstance(item, int):
+        data = item.to_bytes(n, 'big')
+    elif isinstance(item, float) and n == 4:
+        data = struct.pack('>f', item)
+    else:
+        raise TypeError('get_bytes not supported for ' +
+                        str(type(item)) + ' with size ' + n + '.')
+    #data = encode_grey(data)
+    return data
+
+# ref is [num, byte_len]
+def from_grey_bytes(bts, ref_n):
+    #bts = decode_grey(bts)
+    data = 0
+    if isinstance(ref_n, int):
+        data = int.from_bytes(bts, 'big')
+    elif isinstance(ref_n, float) and len(bts) == 4:
+        data = struct.unpack('>f', bts)[0]
+    else:
+        raise TypeError('from_bytes not supported for ' +
+                        str(type(ref_n)) + 'with size ' + len(bts) + '.')
+    return data
+
+# Create genetic string from e.g. [[2, 4], [2.3, 4]]
+# Input: list of couples of item and byte length.
+def list_with_size_to_gs(lst):
+    arr = bytearray(0)
+    for item in lst:
+        arr += bytearray(get_grey_bytes(item[0], item[1]))
+    return arr 
+
+# size is list of byte lengths
+def gs_to_list_with_size(arr_grey, ref_list):
+    arr = copy.copy(arr_grey)
+    lst = copy.deepcopy(ref_list)
+    idx = 0
+    for i in range(len(lst)):
+        ref_n = lst[i][0]
+        size = lst[i][1]
+        n = from_grey_bytes(arr[idx:idx+size], ref_n)
+        lst[i] = [n, size]
+        idx += size
+    return lst
+        
+    
 
 # def byte_len(lst):
 #     tot_len = 0
